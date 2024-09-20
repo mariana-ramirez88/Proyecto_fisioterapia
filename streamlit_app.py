2ec504346aa790981e6001e921b44eefdb242b57
Mariana <mariram2002@gmail.com>
2024-09-20 13:18:40 -0500

    preguntas incluidas

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,22 +7,26 @@ st.title('Encuesta Fisioterapia')
 st.header("Responde las siguientes preguntas:")
 
 # Question
-question = "¿Cuántas horas duermes al día?"
+question = "¿Aceptas el tratamiento de datos personales?"
 st.write(question)
-st.selectbox('Select', ["menos de 5","entre 6 y 8","más de 8"])
-
+st.checkbox("Acepto")
 # User input (dropdown, radio buttons, or text input)
 user_answer = st.radio(
     "¿Cuál es tu edad?:",
     ('10-12', '13-15', '16-18', 'mayor a 18')
 )
 
+user_answer2 = st.radio(
+    "¿Cuántas horas duermes al dia?:",
+    ('5 o menos', '6-8', 'mas de 8')
+)
+
 # Button to submit the answer
 if st.button("Submit"):
     # Scoring logic
     if user_answer == '10-12':
-        score = 100/4
-        st.success(f"Correct! Your score is {score}.")
+        score = 4
+        st.success(f"Tienes un riesgo bajo de tener dolor crónico: {score}.")
     else:
-        score = 0
-        st.error(f"Incorrect! Your score is {score}.")
+        score = 1
+        st.error(f"Tienes un alto riesgo de dolor crónico: {score}.")
