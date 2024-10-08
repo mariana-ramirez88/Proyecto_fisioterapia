3e2a4fbe7ed98dbfb544e6d2ffc5a5d4e8e38c21
Mariana <mariram2002@gmail.com>
2024-10-08 11:42:44 -0500

    preguntas F2

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ user_age = st.radio(
     ('10-12', '13-15', '16-18', 'mayor a 18')
 )
 
+st.subheader('Preguntas Relacionadas al Sueño')
+
 # Question 3: Sleep Factor 5
 valores_respuestasF5 = {
     "sí": 1,
@@ -27,6 +29,7 @@ valores_respuestasF5 = {
 respuestasF5 = []
 
 # Preguntas de sueño
+# SLEEP FACTOR 5
 user_sleepF5_1 = st.radio(
     "¿Despiertas más cansado de lo que estabas antes de acostarte?",
     list(valores_respuestasF5.keys())
@@ -51,10 +54,49 @@ user_sleepF5_4 = st.radio(
 )
 respuestasF5.append(valores_respuestasF5[user_sleepF5_4])
 
-# Calcular el promedio de las respuestas
+# Calcular el promedio de las respuestas 
 if respuestasF5:  
-    promedio = sum(respuestasF5) / len(respuestasF5)
-    st.write("El promedio de tus respuestas es:", promedio)
+    promedioF5 = sum(respuestasF5) / len(respuestasF5)
+    st.write("El promedio de tus respuestas es:", promedioF5)
+
+# SLEEP FACTOR 2
+# Lista para almacenar las respuestas
+respuestasF2 = []
+# opciones de respuesta iguales a F5
+
+user_sleepF2_1 = st.radio(
+    "¿Tuviste pesadillas?",
+    list(valores_respuestasF5.keys())
+)
+respuestasF2.append(valores_respuestasF5[user_sleepF2_1])
+
+user_sleepF2_2 = st.radio(
+    "¿Despertaste con miedo?",
+    list(valores_respuestasF5.keys())
+)
+respuestasF2.append(valores_respuestasF5[user_sleepF2_2])
+
+user_sleepF2_3 = st.radio(
+    "¿Despertaste sudando por algo que soñaste?",
+    list(valores_respuestasF5.keys())
+)
+respuestasF2.append(valores_respuestasF5[user_sleepF2_3])
+
+user_sleepF2_4 = st.radio(
+    "¿Soñaste con algo que te dio miedo?",
+    list(valores_respuestasF5.keys())
+)
+respuestasF2.append(valores_respuestasF5[user_sleepF2_4])
+
+
+# Calcular el promedio de las respuestas 
+if respuestasF2:  
+    promedioF2 = sum(respuestasF2) / len(respuestasF2)
+    st.write("El promedio de tus respuestas es:", promedioF2)
+
+
+
+
 
 # Button to submit the answers
 if st.button("Submit"):
